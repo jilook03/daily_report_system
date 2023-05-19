4361ff28326b7e3e3721741643112ee5d165f39b
goyu.maruyama <goyu.board@gmail.com>
2023-05-19 22:18:19 +0900

    Followモデル作成

--- src/main/java/models/Follow.java
+++ src/main/java/models/Follow.java
@@ -1,39 +1,52 @@
-/*package models;
+package models;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 import constants.JpaConst;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import models.Follow.PK;
 
 @Table(name = JpaConst.TABLE_FOL)
+@NamedQueries({
+    @NamedQuery(
+            name = JpaConst.Q_FOL_CHECK_FOLLOW,
+            query = JpaConst.Q_FOL_CHECK_FOLLOW_DEF),
+    @NamedQuery(
+            name = JpaConst.Q_FOL_UNFOLLOW,
+            query = JpaConst.Q_FOL_UNFOLLOW_DEF),
+    @NamedQuery(
+            name = JpaConst.Q_FOL_GET_FOL,
+            query = JpaConst.Q_FOL_GET_FOL_DEF)
+})
 
+@IdClass(value=PK.class)
 @Getter //全てのクラスフィールドについてgetterを自動生成する(Lombok)
 @Setter //全てのクラスフィールドについてsetterを自動生成する(Lombok)
 @NoArgsConstructor //引数なしコンストラクタを自動生成する(Lombok)
 @AllArgsConstructor //全てのクラスフィールドを引数にもつ引数ありコンストラクタを自動生成する(Lombok)
 @Entity
-public class Follow {
+public class Follow implements Serializable {
 
     @Id
-    @Column(name = JpaConst.FOL_COL_ID)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    @Column(name = JpaConst.FOL_COL_FROM_ID, nullable = false)
+    private Integer followFromId;
 
-    @Column(name = JpaConst.REP_COL_EMP, nullable = false)
-    private Employee Employee;
-
-    @Column(name = JpaConst.FOL_COL_FOLLOWED, nullable = false)
-    private String id;
+    @Id
+    @Column(name = JpaConst.FOL_COL_TO_ID, nullable = false)
+    private Integer followToId;
 
     @Column(name = JpaConst.FOL_COL_CREATED_AT, nullable = false)
     private LocalDateTime createdAt;
@@ -41,5 +54,9 @@ public class Follow {
     @Column(name = JpaConst.FOL_COL_UPDATED_AT, nullable = false)
     private LocalDateTime updatedAt;
 
+    @Data
+    public static class PK implements Serializable {
+        private Integer followFromId;
+        private Integer followToId;
+    }
 }
-*/
